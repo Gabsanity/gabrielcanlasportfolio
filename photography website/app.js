90aaec5e54ea9e4f5c4be512ddba4fd745917482
Gabsanity <65479530+Gabsanity@users.noreply.github.com>
2021-01-19 11:07:23 +0800

    first commit

---
+++ photography website/app.js
@@ -0,0 +1,41 @@
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const links = nav.querySelectorAll("a");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
+  });
+});
+
+const text = document.querySelector(".fancy");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = text.querySelectorAll("span")[char];
+  span.classList.add("fade");
+  char++;
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
